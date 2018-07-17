ea579b86ffa4f6465054561be914c532add77128
sagar <sagar_sys@live.com>
2018-07-18 01:14:38 +0400

    -- modify express server port + add some sample response

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.json([{
+        id: 1,
+        name: "Hiccup",
+        password: 'hiccup'
+    }, {
+        id: 2,
+        name: "King Arthur",
+        password: 'king-arthur'
+    }]);
 });
 
 module.exports = router;
